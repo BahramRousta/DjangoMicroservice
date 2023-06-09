55fd7b2ea8cdc1881d576b2d93bd8d8c74725f54
bahramrousta <rousta.bahram@gmail.com>
2023-06-09 17:58:46 +0330

    change auth gateways name

--- gateway/user_gateway/views.py
+++ gateway/user_gateway/views.py
@@ -1,21 +1,21 @@
 from core.views import GateWayView
 
 
-class AuthLoginView(GateWayView):
+class AuthLoginGateWay(GateWayView):
 
     def create(self, request):
         url = 'http://127.0.0.1:8002/login/'
         return self._send_request(request, url)
 
 
-class AuthRegisterView(GateWayView):
+class AuthRegisterGateWay(GateWayView):
 
     def create(self, request):
         url = 'http://127.0.0.1:8002/register/'
         return self._send_request(request, url)
 
 
-class AuthLogOutView(GateWayView):
+class AuthLogOutGateWay(GateWayView):
 
     def create(self, request):
         url = 'http://127.0.0.1:8002/logout/'
